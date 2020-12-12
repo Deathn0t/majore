ca533c2e3962cbafe78eab4ab43f7e701da88771
Jeremy Gozlan <jeremy.goz17@gmail.com>
2020-12-12 13:19:54 +0100

    reshapes + encoder changes

--- scripts/data/encoders.py
+++ scripts/data/encoders.py
@@ -9,7 +9,7 @@ class AudioEncoder(nn.Module):
 
     def __init__(self, audio_dim, audio_size, tied_output, nhead, nlayer, d_model, d_feedforward, dropout, down_sampling_factor):
         super(AudioEncoder, self).__init__()
-
+        
         self.fc1 = nn.Linear(audio_dim, tied_output)
 
         self.pos_enc = PositionalEncoding(d_model = tied_output, max_len = audio_size, dropout = dropout)
@@ -20,9 +20,14 @@ class AudioEncoder(nn.Module):
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers = nlayer)
 
     def forward(self, x):
+        
         x = F.relu(self.fc1(x))
         x = self.pos_enc(x)
         x = self.down_sampling(x)
+        
+        batch, seq_len, dim = x.shape[0], x.shape[1], x.shape[2]
+        x = x.view(seq_len, batch, dim)
+        
         x = self.transformer_encoder(x)
         return x
 
@@ -39,6 +44,10 @@ class VideoEncoder(nn.Module):
     def forward(self, x):
 
         x = F.relu(self.fc1(x))
+        
+        batch, seq_len, dim = x.shape[0], x.shape[1], x.shape[2]
+        x = x.view(seq_len, batch, dim)
+        
         x = self.transformer_encoder(x)
         return x
 
@@ -56,29 +65,55 @@ class DotProductAttention(nn.Module):
         self.w_ks = nn.Linear(d_model, d_k, bias=False)
         self.w_vs = nn.Linear(d_model, d_v, bias=False)
 
-
     def forward(self, q , k, v):
+        
+        #IN
+        #query: :math:`(L, N, E)` where L is the target sequence length, N is the batch size, E is
+        # the embedding dimension.
+        #- key: :math:`(S, N, E)`, where S is the source sequence length, N is the batch size, E is
+          #the embedding dimension.
+        #- value: :math:`(S, N, E)` where S is the source sequence length, N is the batch size, E is
+          #the embedding dimension.
+           
+        #OUT 
+        #attn_output: :math:`(L, N, E)` where L is the target sequence length, N is the batch size,
+        #E is the embedding dimension.
+            
+        # IN OUR CASE    
+        #torch.Size([1081, 10, 480])
+        #torch.Size([1, 10, 480])
+        #torch.Size([1, 10, 480])
 
         d_k, d_v =  self.d_k, self.d_v
 
-        sz_batch, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+        len_q, sz_batch, emb_dim =  q.size(0), q.size(1), q.size(2)
 
         residual = q
-
-        # Pass through the pre-attention projection: b x lq x dv
-
-        q = self.w_qs(q).view(sz_batch, len_q, d_k)
-        k = self.w_ks(k).view(sz_batch, len_k, d_k)
-        v = self.w_vs(v).view(sz_batch, len_v, d_v)
-
-        # Transpose for attention dot product: b x lq x dv
-
-        attn = torch.matmul(q, k.transpose(1, 2))
+        
+        q = self.w_qs(q).view(len_q, sz_batch, d_k).transpose(0,1)
+        k = self.w_ks(k).view(-1, sz_batch, d_k).transpose(0,1)
+        v = self.w_vs(v).view(-1, sz_batch, d_v).transpose(0,1)
+        
+        #torch.Size([10, 1081 480])
+        #torch.Size([10,1, 480])
+        #torch.Size([10,1, 480])
+        
+        attn = torch.matmul(q, k.transpose(1,2))
+        
+        #torch.Size([10, 1081, 1])
 
         attn = F.softmax(attn, dim=-1)
+        
+        #torch.Size([10, 1081, 1])
 
         output = torch.matmul(attn, v)
-
+        
+        #torch.Size([10, 1081, 480])
+        
+        output = output.transpose(0, 1).contiguous().view(len_q, sz_batch, emb_dim)
+        
+        #torch.Size([1081, 10, 480])
+       
         output = output* self.alpha + (1.0 - self.alpha) * residual
 
         return output
